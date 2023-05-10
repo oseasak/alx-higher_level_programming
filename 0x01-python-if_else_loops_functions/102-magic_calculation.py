297da3b4dc4165c7511217fb8d30d08f4f196ea6
PateK <107476874+oseasak@users.noreply.github.com>
2023-05-10 13:19:55 +0100

    Create 102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author -Asak Erhumuose
+
+def magic_calculation(a, b, c):
+    """bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
