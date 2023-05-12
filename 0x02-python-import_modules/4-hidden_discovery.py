c47fd9d159fd80523b1be1d96e5e10869c49fb5d
PateK <107476874+oseasak@users.noreply.github.com>
2023-05-13 00:02:10 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-if __nam__ == "__main__":
+if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    names = dir(hidden_4)
-    for nam in names:
-        if nam[:2] != "__":
-            print(nam)
+    nams = dir(hidden_4)
+    for name in nams:
+        if name[:2] != "__":
+            print(name)
